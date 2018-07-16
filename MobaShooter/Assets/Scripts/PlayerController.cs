606ca5be96837bfb1913dad5c217f838f33e7f57
GreedyCoding <sers.noreply.github.comsers.noreply.github.com>
2018-07-16 02:04:21 +0200

    Started adding the PlayerController und PlayerMotor

---
+++ MobaShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    //SerializeField makes it editable from the Unity Editor
+    private float speed = 5f;
+
+    //Setting a reference to the PlayerMotor
+    private PlayerMotor motor;
+
+    private void Start() {
+
+        //Get the playermotor component
+        motor = GetComponent<PlayerMotor>();
+
+    }
+
+    private void Update() {
+
+    //Calculate movement velocity as Vector3
+
+        //Gets a vetcor based on the input(Keyboard and Controller)
+        // W-UP    ( 1, 0, 0)
+        // S-DOWN  (-1, 0, 0)
+        // A-LEFT  ( 0, 0, 1)
+        // D-RIGHT ( 0, 0,-1)
+        float _xMovement = Input.GetAxisRaw("Horizontal");
+        float _zMovement = Input.GetAxisRaw("Vertical");
+
+        //transform.right/forward takes the current rotation into consideration
+        Vector3 _moveHorizontal = transform.right * _xMovement;
+        Vector3 _moveVertical = transform.forward * _zMovement;
+
+        //Adding both movement axis together and normalizing them so they only serve for direction
+        //Multiplying the direction by the speed so we can control the movespeed
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        //Let the Motor move with the desired calculated velocity
+        motor.Move(_velocity);
+
+    //Calculate rotation as a Vector3
+
+    }
+
+}
---
+++ MobaShooter/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate() {
+
+        PerformMovement();
+
+    }
+
+
+    //Gets a movement vector
+    public void Move(Vector3 _velocity) {
+
+        velocity = _velocity;
+
+    }
+
+    void PerformMovement() {
+
+        if (velocity != Vector3.zero) {
+
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        }
+    }
+
+}
