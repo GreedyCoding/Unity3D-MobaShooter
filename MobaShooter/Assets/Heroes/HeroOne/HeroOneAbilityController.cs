0daa55893fd65b3aaf86932fe7dca859e8c4b06a
GreedyCoding <marco_hofer2012@outlook.com>
2018-10-16 22:37:09 +0200

    Cleaned up the Hierarchy and started the Ability Controller for the first hero representing tracers abilities for now

---
+++ MobaShooter/Assets/Heroes/HeroOne/HeroOneAbilityController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroOneAbilityController : AbilityController {
+
+    private float blinkDistance = 10f;
+
+    //Blink
+    public override void AbilityOne()
+    {
+        RaycastHit hit;
+        //Destination we want to hit if we are not intersecting anything
+        Vector3 destination = transform.position + transform.forward * blinkDistance;
+
+        //Casting a linecast ray to the destination to check if we are intersecting anything
+        if (Physics.Linecast(transform.position, destination, out hit))
+        {
+            //If we hit anything we are going to set the blinkdestination to the distance of the hit -1
+            destination = transform.position + transform.forward * (hit.distance - 1f);
+        }
+
+        if (Physics.Raycast(destination, -Vector3.up, out hit))
+        {
+            destination = hit.point;
+            destination.y = 1f;
+            transform.position = destination;
+        }
+
+    }
+
+    //Recall
+
+
+    //Bomb
+}
---
+++ MobaShooter/Assets/Scripts/Player/AbilityController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityController : MonoBehaviour
+{
+    public virtual void AbilityOne()
+    {
+
+    }
+
+    public virtual void AbilityTwo()
+    {
+
+    }
+
+    public virtual void Ultimate()
+    {
+
+    }
+}
